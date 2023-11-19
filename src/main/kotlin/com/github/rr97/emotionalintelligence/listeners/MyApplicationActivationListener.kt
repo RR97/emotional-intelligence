34acba5cd71cd383266011735dc6790eaa791bb3
GitHub Action <action@github.com>
2023-11-19 06:40:30 +0000

    Template cleanup

--- src/main/kotlin/com/github/rr97/emotionalintelligence/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/rr97/emotionalintelligence/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.rr97.emotionalintelligence.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
--- src/main/kotlin/com/github/rr97/emotionalintelligence/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/rr97/emotionalintelligence/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.rr97.emotionalintelligence.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.rr97.emotionalintelligence.MyBundle
+import com.github.rr97.emotionalintelligence.services.MyProjectService
 import javax.swing.JButton
 
 
